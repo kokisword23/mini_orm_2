cea6729a99216cd56d1a84ebd3e624828cce106e
Lancelot <kokisword23@gmail.com>
2024-02-19 20:00:44 +0200

    Mini orm part 2 finished

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import entities.Order;
+import entities.Product;
+import entities.User;
+import orm.EntityManager;
+import orm.MyConnector;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException {
+        MyConnector.createConnection("root", "1234", "mini_orm");
+        Connection connection = MyConnector.getConnection();
+
+        EntityManager<User> userEntityManager = new EntityManager<>(connection);
+//        userEntityManager.doCreate(User.class);
+        User yomov = userEntityManager.findFirst(User.class);
+        userEntityManager.delete(yomov);
+//        userEntityManager.doAlter(yomov);
+//        userEntityManager.persist(yomov);
+//
+//        User users = userEntityManager.findFirst(User.class);
+//
+//        System.out.println(users);
+
+//        EntityManager<Product> productEntityManager = new EntityManager<>(connection);
+//        Product pen = new Product("pen", 2.34);
+//        productEntityManager.persist(pen);
+//
+//        EntityManager<Order> orderEntityManager = new EntityManager<>(connection);
+//        orderEntityManager.doCreate(Order.class);
+//        Order order = new Order("mn123b4", LocalDate.now());
+//        orderEntityManager.persist(order);
+    }
+}
